b32e72d5cb52c3f0a53ad3b000926f3dd4ccbfc3
NevestarMz <nevestar@nevestar.co.mz>
2025-08-01 14:35:50 +0200

    Atualização do Projecto de Deploy NeveStar!

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,6 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta name="description" content="A NeveStar oferece soluções em tecnologia, software e segurança digital em Moçambique.">
+    <meta name="keywords" content="tecnologia, Inovação, software, Moçambique, segurança digital, desenvolvimento web">
+    <meta name="author" content="NeveStar - Inova & Cria">
+    <meta name="robots" content="index, follow">
+    <link rel="canonical" href="https://nevestar.co.mz/">
+
+    <meta property="og:title" content="NeveStar - Inovação em Tecnologia">
+    <meta property="og:description" content="Soluções digitais e tecnológicas para empresas e projectos em Moçambique.">
+    <meta property="og:image" content="https://nevestar.co.mz/imagens/og-image.jpg">
+    <meta property="og:url" content="https://nevestar.co.mz">
+    <meta property="og:type" content="Website">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <title>NeveStar</title>
