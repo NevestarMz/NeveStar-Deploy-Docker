821959e398b456a3ae97ebd2b973090084b0f01a
NevestarMz <nevestar@nevestar.co.mz>
2025-08-03 00:45:17 +0200

    Atualização do Projecto de Deploy NeveStar!

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <!--  -->
+    <meta name="google-site-verification" content="Mq4sMxiYtAScWdqTMnsxoESOA5pXuGE7MoGjXNOHB2Y" />
+
     @yield('title')    
     <!-- <title>NeveStar - Soluções Tecnológicas e Inovadoras em Moçambique</title> -->
     
